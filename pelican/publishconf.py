455f66b6d51e29fe4fe40a4414afdbe0cbc9931f
40423218 <40423218@gmaul.com>
2016-01-15 02:19:54 +0800

    030

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = '40423218'
-SITENAME = '2015FALL 40423218 CBP 作業'
+SITENAME = '2015FALL 40423218 CPB 作業'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,7 +26,8 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),
+         ('40423218-gh-pages','http://40423218.github.io/40423218/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
